d96991d98105b62b223add9e9f8fca1f20ff0159
felipe6ouza <felipe6ouza@outlook.com>
2021-04-21 15:21:29 -0300

    Configuração de estrutura para Entity Framework Core.

---
+++ src/Productry.Data/Context/ProductryDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Productry.Bussiness.Models;
+
+namespace Productry.Data.Context
+{
+    public class ProductryDbContext : DbContext
+    {
+        public ProductryDbContext(DbContextOptions<ProductryDbContext> options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductryDbContext).Assembly);
+        }
+    }
+
+}
---
+++ src/Productry.Data/Mappings/ProdutoMapping.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Productry.Bussiness.Models;
+
+namespace Productry.Data.Mappings
+{
+    public class ProdutoMapping : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.ToTable("Produtos");
+
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Nome).HasColumnType("VARCHAR(200)").IsRequired();
+            builder.Property(p => p.Data_Cadastro).HasColumnType("Date");
+            builder.Property(p => p.Valor_Unitario).HasPrecision(12, 2);
+        }
+    }
+}
