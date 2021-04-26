491d1a34864afa71ae84d3464215e9d225297abd
felipe6ouza <felipe6ouza@outlook.com>
2021-04-25 21:32:27 -0300

    Altera configuração do AutoMapper para detalhar mapeamento.

--- src/Productry.API/Configurations/AutoMapperConfig.cs
+++ src/Productry.API/Configurations/AutoMapperConfig.cs
@@ -4,12 +4,27 @@ using Productry.Bussiness.Models;
 
 namespace Productry.API.Configurations
 {
-    public class AutoMapperConfig : Profile
+    public class BusinessEntityToViewModel : Profile
     {
-        public AutoMapperConfig()
+        public BusinessEntityToViewModel()
         {
-            CreateMap<Produto, ProdutoViewModel>().ReverseMap();
-            
+            CreateMap<Produto, ProdutoViewModel>();
+            CreateMap<Cartao, CartaoViewModel>();
+            CreateMap<Compra, CompraViewModel>();
+        }
+
+    public class ViewModelToBusinessEntity : Profile
+    {
+        public ViewModelToBusinessEntity()
+        {
+            CreateMap<ProdutoViewModel, Produto>()
+                    .ConstructUsing(p=> new Produto(p.Nome, p.ValorUnitario, p.QtdeEstoque));
+
+            CreateMap<CartaoViewModel, Cartao>()
+                   .ConstructUsing(c => new Cartao(c.Titular, c.Numero, c.DataExpiracao, c.Bandeira,c.Cvv));
+
+            CreateMap<CompraViewModel, Compra>()
+                   .ConstructUsing(c => new Compra(c.ProdutoId, c.QtdeComprada, c.Cartao));
         }
     }
 }
