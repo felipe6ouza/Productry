73c3cd612b11bf985ffa12d236c62fb13aeac117
felipe6ouza <felipe6ouza@outlook.com>
2021-04-27 14:12:02 -0300

    Altera ColumnType do Valor Unitário de um produto.

---
+++ src/Productry.Data/Migrations/20210427163225_AlterTypeValor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Productry.Data.Migrations
+{
+    public partial class AlterTypeValor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ValorUnitario",
+                table: "Produtos",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Valor",
+                table: "Pagamentos",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "ValorUnitario",
+                table: "Produtos",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Valor",
+                table: "Pagamentos",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
